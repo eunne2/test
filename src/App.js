8e3cc4560a94ddd89b4bc7f9dc55304261155fe8
eunne2 <ehye518@gmail.com>
2023-03-06 16:05:12 +0900

    파일 정리 및 작업 준비

--- src/App.js
+++
@@ -1,5 +0,0 @@
-function App() {
-  return <div>haha</div>;
-}
-
-export default App;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,14 @@
+import styles from "./Home.module.css";
+
+function Home() {
+  return (
+    <div>
+      <div>네비게이션 바</div>
+      <div>메인 내용</div>
+      <div>푸터</div>
+      <p>얘네를 다 컴포넌트로 해서 쓰기</p>
+    </div>
+  );
+}
+
+export default Home;
