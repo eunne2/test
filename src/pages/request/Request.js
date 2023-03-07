606a2e768ff2c6b1afb6c6807ee575164ce0e677
eunne2 <ehye518@gmail.com>
2023-03-07 15:36:15 +0900

    mysql 다운로드, db 가져올 준비

--- src/pages/request/Request.js
+++ src/pages/request/Request.js
@@ -6,10 +6,10 @@ function Request() {
   return (
     <div>
       <Category category={"신고 / 순찰 요청"} text={"순찰요청"} />
-      <div>
-        <div>순찰이 필요하다면 언제든지 삐용이에게 요청하세요!</div>
-        <Button selectBtn={1} text={"작성하기"} />
+      <div className={styles.title}>
+        순찰이 필요하다면 언제든지 삐용이에게 요청하세요!
       </div>
+      <Button selectBtn={1} text={"작성하기"} />
     </div>
   );
 }
