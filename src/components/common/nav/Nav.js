2d2fccc2b01e4da9a507ac956eacc290dcb0e423
eunne2 <ehye518@gmail.com>
2023-03-08 15:11:17 +0900

    임시 Nav 컴포넌트 생성

---
+++ src/components/common/nav/Nav.js
@@ -0,0 +1,7 @@
+import styles from "./Nav.module.css";
+
+function Nav() {
+  return <nav className={styles.nav}>PIYONG</nav>;
+}
+
+export default Nav;
--- src/pages/request/Request.js
+++ src/pages/request/Request.js
@@ -2,10 +2,12 @@ import styles from "./Request.module.css";
 import Category from "../../components/common/category/Category";
 import Button from "../../components/common/button/Button";
 import Posts from "../../server/config/Posts";
+import Nav from "../../components/common/nav/Nav";
 
 function Request() {
   return (
     <div>
+      <Nav />
       <Category category={"신고 / 순찰 요청"} text={"순찰요청"} />
       <div className={styles.title}>
         순찰이 필요하다면 언제든지 삐용이에게 요청하세요!
